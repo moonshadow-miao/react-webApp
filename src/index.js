d1d3edb0d649696cf378dfa3f2038a64fde4bd0a
moonshadow_miao <1024104763@qq.com>
2017-12-07 16:40:40 +0800

    引入fastclick

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './assets/js/fastclick'
+
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    window.FastClick.attach(document.body);
+  }, false);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
